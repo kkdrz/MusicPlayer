f9824cb9e530eb5eb88fc2fc328840ce43d7727e
kkdrz <drozdkonrad@gmail.com>
2017-06-10 23:10:02 +0200

    Change layout. Blurred background, more dark colors ;>

--- app/src/main/java/pl/edu/pwr/drozd/musicplayer/PlayerActivity.java
+++ app/src/main/java/pl/edu/pwr/drozd/musicplayer/PlayerActivity.java
@@ -1,14 +1,18 @@
 package pl.edu.pwr.drozd.musicplayer;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
-
+import com.enrique.stackblur.StackBlurManager;
+import java.util.ArrayList;
 import javax.inject.Inject;
-
 import butterknife.BindView;
-import pl.edu.pwr.drozd.musicplayer.dagger.AppComponent;
+import butterknife.ButterKnife;
+import pl.edu.pwr.drozd.musicplayer.dataModel.Song;
 
 public class PlayerActivity extends AppCompatActivity {
 
@@ -20,12 +24,25 @@ public class PlayerActivity extends AppCompatActivity {
     @BindView(R.id.player_layout)       ViewGroup mPlayerLayout;
 
     @Inject PlaylistManager playlistManager;
+    ArrayList<Song> mPlaylist;
+
+    public final static int BACKGROUND_IMAGE = R.drawable.background;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         ((MyApp) getApplication()).getAppComponent().inject(this);
+        ButterKnife.bind(this);
+        mPlaylist = playlistManager.getPlaylist();
+
+        setBlurryBackground();
+    }
+
+    private void setBlurryBackground() {
+        Bitmap bm = BitmapFactory.decodeResource(getResources(), BACKGROUND_IMAGE);
+        StackBlurManager blurManager = new StackBlurManager(bm);
+        blurManager.process(20);
+        mPlayerLayout.setBackground(new BitmapDrawable(getResources(), blurManager.returnBlurredImage()));
     }
 }
